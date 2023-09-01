2ae0d3ca57e20dfc3dc2b3e481d0999427e4d4d9
Akst Elena <akstelena@yandex.ru>
2023-09-01 16:57:34 +0300

    Seminar3

--- Seminar/Seminar3/Seminar32.java
+++ Seminar/Seminar3/Seminar32.java
@@ -1,10 +1,11 @@
 package Seminar.Seminar3;
+//  Создайте класс Сетчик, у которого есть метод add(), увеличивающий значение внутренней int переменной на 1. Сделайте так, чтобы с объектом такого типа можно было работать в блоке try-with-resources. Нужно бросить исключение, если работа с объектом типа счетчик была не в ресурсном try и/или ресурс остался открытым. Подумайте какой тип исключения подойдет луше всего.
 
 public class Seminar32 {
   public static void main(String[] args) {
     try (Counter count = new Counter()) {
       count.add();
-      // count.close();
+      count.close(); // чтобы вызвать ошибку
       count.printCount();
     } catch (Exception e) {
       System.err.println("error");
---
+++ Seminar/Seminar3/ZeroDivException.java
@@ -0,0 +1,8 @@
+package Seminar.Seminar3;
+
+public class ZeroDivException extends ArithmeticException {
+
+  public ZeroDivException(String msg) {
+    super(msg);
+  }
+}
